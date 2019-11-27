e66b3ccce20c97fd646013c9b958242b4aad4957
KennethThNg <43238220+KennethThNg@users.noreply.github.com>
2019-11-27 14:27:43 +0100

    NewLayer

--- DynamicalSystemLayer.py
+++ DynamicalSystemLayer.py
@@ -3,6 +3,83 @@ import torch.nn as nn
 from torch.autograd import *
 import math
 
+class LinearODELayer(nn.Module):
+    def __init__(self, in_features, out_features, bias=False):
+        super(LinearODELayer, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = nn.Parameter(torch.Tensor(self.out_features, self.in_features))
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(self.out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+    # the forward step
+    def forward(self, x):
+        output = x.mm(self.weight)
+        if self.bias is not None:
+            output += self.bias
+        return output
+    # the backward function is not necessary, sincewe have only used
+    # functions from pytorch (in particular, .reshape and .mm)
+    
+    # this function is inherited from the pytorch class Linear
+    def reset_parameters(self):
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in)
+            nn.init.uniform_(self.bias, -bound, bound)
+            
+#Thsi object can be extended
+class NonLinearODELayer(nn.Module):
+    def __init__(self, in_dim, hid_dim, out_dim):
+        super(NonLinearODELayer, self).__init__()
+        self.in_dim = in_dim
+        self.weight = nn.Parameter(torch.Tensor(self.in_dim, self.in_dim, self.in_dim))
+    
+    def forward(self, x):
+        xBx = x.matmul(self.weight).matmul(x.t()).permute(2,1,0)
+        out = torch.diagonal(xBx).t()
+        return out
+    
+    def reset_parameters(self):
+        init.kaiming_uniform_(self.weight, a=math.srqt(5))
+        
+class LinearODEModel(nn.Module):
+    def __init__(self, in_feature, out_feature, bias=False):
+        super(LinearODEModel, self).__init__()
+        self.in_feature = in_feature
+        self.out_feature = out_feature
+        self.bias = bias
+        self.ode = LinearODELayer(self.in_feature, self.out_feature, self.bias)
+        
+    def forward(self, x):
+        out = self.ode(x)
+        return out
+    
+    
+class NNODEModel(nn.Module):
+    def __init__(self, in_dim, hid_dim, out_dim):
+        super(NNODEModel, self).__init__()
+        #Dimension
+        self.in_dim = in_dim
+        self.hid_dim = hid_dim
+        self.out_dim = out_dim
+        
+        #Layer 
+        self.lin_ode = LinearODELayer(self.in_dim, self.out_dim)
+        self.nl_ode = NonLinearODELayer(self.in_dim, self.hid_dim, self.out_dim)
+        
+        #weight init
+        self.lin_ode.weight.data.uniform_(-0.1,0.1)
+        self.nl_ode.weight.data.uniform_(-0.001,0.001)
+            
+        
+    def forward(self, x):
+        out = self.lin_ode(x) + self.nl_ode(x)
+        return out
+
 def to_np(x):
     return x.detach().cpu().numpy()
 
